e37570757810f6f11e55fce5f2702488de166086
digitalz4k <leodsirs@gmail.com>
2013-07-20 21:15:05 +0200

    Initial push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coachvincent::Application.config.secret_token = '7451279cfb0516cc742a5a12b8655070ec56b03456993e2b5b4edd4fa35db9cecffb5aa5205439362fd967f5a0e00d0141eec469825b1c2013301dd49fb5bd8e'
