49c988c4cf4756d4f130ba3970abec29cb72ccdf
digitalz4k <leodsirs@gmail.com>
2013-07-24 04:27:27 +0200

    Test fonts updates production rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Coachvincent::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   # Add the fonts path
-  config.assets.paths << "#{Rails.root}/app/assets/fonts"
+  config.assets.paths << Rails.root.join("app", "assets", "fonts") 
 
   # Precompile additional assets
   config.assets.precompile += %w( .svg .eot .woff .ttf )
