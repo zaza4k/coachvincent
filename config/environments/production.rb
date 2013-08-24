2368f100c1e24173d6ad8ac88e5894b2b58d5676
digitalz4k <leodsirs@gmail.com>
2013-08-24 22:44:51 +0200

    Heroku precompile fix.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Coachvincent::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
 
   # Add the fonts path
