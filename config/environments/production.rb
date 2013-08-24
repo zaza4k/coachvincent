9b10ff3a3a1340e2a779f791c99ce6a7446f2040
digitalz4k <leodsirs@gmail.com>
2013-08-24 23:21:49 +0200

    Heroku fix 004.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Coachvincent::Application.configure do
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-  config.action_dispatch.x_sendfile_header = ni
+  config.action_dispatch.x_sendfile_header = nil
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -70,6 +70,4 @@ Coachvincent::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-
-
 end
